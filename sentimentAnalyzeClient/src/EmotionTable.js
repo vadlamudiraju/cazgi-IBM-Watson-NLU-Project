2c8fad8de87446277842d2817ebfdc8356df3f96
vadlamudiraju <188w1a01h0@gmail.com>
2022-12-07 20:34:52 +0530

    changes made from theia lab environment

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -12,6 +12,14 @@ class EmotionTable extends React.Component {
               /*Write code to use the .map method that you worked on in the 
               Hands-on React lab to extract the emotions. If you are stuck,
               please click the instructions to see how to implement a map*/
+              this.props.emotions && Object.entries(this.props.emotions).map(function (element,val) {
+                return (
+                  <tr key={element}>
+                    <td>{element[0]}</td>
+                      <td>{element[1]}</td>
+                  </tr>
+                );
+               })
             }
             </tbody>
           </table>
